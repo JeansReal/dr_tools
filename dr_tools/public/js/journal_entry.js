a73c832d99f1294f3f586a1b7901d805b856885c
Jeans K. Real <jkrb7@hotmail.com>
2025-07-03 09:54:26 -0600

    Accounting Dimensions working!

--- dr_tools/public/js/journal_entry.js
+++ dr_tools/public/js/journal_entry.js
@@ -3,7 +3,8 @@ frappe.ui.form.on('Journal Entry Account', {
 	account(frm, cdt, cdn) {
 		let row = locals[cdt][cdn];
 
-		frm.set_df_property('accounts', 'reqd', row.account_type === 'Bank', frm.doc.name, 'bank_account', row.name);
+		// If there is Account and account_type is 'Bank', then reqd. Else, not reqd.
+		frm.set_df_property('accounts', 'reqd', (row.account && row.account_type === 'Bank'), frm.doc.name, 'bank_account', row.name);
 
 		if (row.account_type !== 'Bank')
 			return; // If is not a Bank Account, we don't need to fetch the bank account name
@@ -12,5 +13,30 @@ frappe.ui.form.on('Journal Entry Account', {
 			row.bank_account = doc ? doc.name : '';
 			refresh_field('bank_account', row.name, 'accounts');
 		});
+	},
+
+	ruta(frm, cdt, cdn) {
+		let row = locals[cdt][cdn];
+
+		frm.set_df_property('accounts', 'reqd', row.ruta, frm.doc.name, 'branch', row.name);
+
+		if (row.ruta) {
+			frappe.db.get_value('Territory', row.ruta, 'parent_territory', (doc) => {
+				row.branch = doc ? doc.parent_territory : '';
+				refresh_field('branch', row.name, 'accounts');
+			});
+		} else {
+			row.branch = '';
+			refresh_field('branch', row.name, 'accounts');
+		}
+	},
+
+	branch(frm, cdt, cdn) {
+		let row = locals[cdt][cdn];
+
+		frm.set_df_property('accounts', 'reqd', row.branch, frm.doc.name, 'branch', row.name);
+
+		row.ruta = '';
+		refresh_field('ruta', row.name, 'accounts');
 	}
 });
